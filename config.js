1f3679ac98bd3eeccc6efe20c00a3b5531dd6e76
Regan Odhiambo Ayiecho <r.ayiecho@alustudent.com>
2026-06-08 14:31:54 +0200

    Add config.js back - Firebase keys are public by design

---
+++ config.js
@@ -0,0 +1,8 @@
+window.AFRIVID_CONFIG = {
+  apiKey: "AIzaSyBDgcY4SYAOdG2QCPZYCEJRPaQNQZm6BI0",
+  authDomain: "afrivid-studio.firebaseapp.com",
+  projectId: "afrivid-studio",
+  storageBucket: "afrivid-studio.firebasestorage.app",
+  messagingSenderId: "957196729758",
+  appId: "1:957196729758:web:0a46ed103675f741e22a8f"
+};
